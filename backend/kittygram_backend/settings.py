acbe510d2b2b0e8b9a0d8d4965ee2083f992ee70
Ilya Kosmakov <kosmakovie@gmail.com>
2023-11-27 10:47:45 +0300

    fix settings

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,6 +1,11 @@
 # flake8: noqa
 import os
+from distutils.util import strtobool
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
